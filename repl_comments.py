b125bb3816cc0910ab7e90f0f3eeeeed57d1ca3b
mursel <yesildal17>
2018-12-21 03:49:55 +0900

    added code for password hashing to the file  repl_comments.py

--- repl_comments.py
+++ repl_comments.py
@@ -1,6 +1,7 @@
 from hashlib import sha256 #***
 all_comments = ''
 order = 1
+my_hash = 'b1e79532d5b97c775ed657e6aea1de1cb3728443c6b35ad80fa7c3c4ed0f4d1e' #***# i created this hash using code in this site: (https://bitbucket.org/damienjadeduff/hashing_example/raw/master/hash_password.py)
 def create_hash(password): #***
     pw_bytestring = password.encode()  #***
     return sha256(pw_bytestring).hexdigest() #***
@@ -13,7 +14,7 @@ def comments(n):
 	else:
 		pw = input('Please enter your password:')  # ***
 		hsh = create_hash(pw)  #***
-		if hsh == 'b1e79532d5b97c775ed657e6aea1de1cb3728443c6b35ad80fa7c3c4ed0f4d1e': #***
+		if hsh == my_hash: 
 			all_comments += '\n' + str(order) + '.' + ' ' + str(n)
 			print('Previously entered comments: ',all_comments)
 			order += 1
